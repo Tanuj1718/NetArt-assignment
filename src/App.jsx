4e7b1755a97781a38f39fbd326c44b439499a0c7
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-02-27 20:30:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import Header from './pages/Header'
-import Section from './pages/Section'
+
 import Section2 from './pages/Section2'
 import Footer from './pages/Footer'
-
+import Section from './pages/Section'
 function App() {
 
   return (
